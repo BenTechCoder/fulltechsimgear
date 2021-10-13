599a8ad6180d43d924ad072aafc7effd192bb626
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2021-10-13 17:24:32 -0400

    added basic functionality to guides blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
-  eleventyConfig.addPassthroughCopy("./src/assets/")
+  eleventyConfig.addPassthroughCopy("./src/assets/");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
 
   return {
     dir: {
