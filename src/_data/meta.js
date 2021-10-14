099182cd523021556d1149e9fbcf37de14705ef5
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2021-10-14 09:23:07 -0400

    got rid of url for meta

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,9 +1,9 @@
 module.exports = {
   // NOTE: `process.env.URL` is provided by Netlify, and may need
   // adjusted pending your host
-  url: process.env.URL || "http://localhost:8080",
-  siteName: "Smol 11ty Starter",
+  // url: process.env.URL || "http://localhost:8080",
+  siteName: "FullTech Sim Gear",
   siteDescription:
-    "Extremely minimal Eleventy starter to kickstart a simple multi-page site / a nearly opinionless foundation to continue building on.",
-  authorName: "Stephanie Eckles", // optional
+    "A Website dedicated guiding you on your sim gear and creative journey",
+  authorName: "Content by Nathan Website by Ben", // optional
 };
